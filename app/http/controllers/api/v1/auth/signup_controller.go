f0701d79dd98984d677eb6aa816536cf7785100d
陈飞 <chenfei@360tianma.com>
2022-10-05 00:20:37 +0800

    验证器

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -3,6 +3,7 @@ package auth
 import (
 	v1 "contract/app/http/controllers/api/v1"
 	"contract/app/models/user"
+	"contract/app/requests"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -14,11 +15,8 @@ type SignupController struct {
 }
 
 func (sc *SignupController) IsPhoneExist(c *gin.Context) {
-	// 请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-	request := PhoneExistRequest{}
+	// 初始化请求对象
+	request := requests.SignupPhoneExistRequest{}
 
 	// 解析json请求
 	if err := c.ShouldBindJSON(&request); err != nil {
@@ -30,6 +28,17 @@ func (sc *SignupController) IsPhoneExist(c *gin.Context) {
 		fmt.Println(err.Error())
 		return
 	}
+
+	// 表单验证
+	errs := requests.ValidateSignupPhoneExist(&request, c)
+	if len(errs) > 0 {
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+		return
+	}
+
+	// 检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
---
+++ app/requests/signup_request.go
@@ -0,0 +1,35 @@
+package requests
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+)
+
+type SignupPhoneExistRequest struct {
+	Phone string `json:"phone,omitempty" valid:"phone"`
+}
+
+func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
+	// 自定义验证规则
+	rules := govalidator.MapData{
+		"phone": []string{"required", "digits:11"},
+	}
+
+	// 自定义验证出错提示
+	messages := govalidator.MapData{
+		"phone": []string{
+			"required:手机必填",
+			"digits:手机号长度为11位数字",
+		},
+	}
+
+	// 配置初始化
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid", // 模型中的struct标签标识符
+		Messages:      messages,
+	}
+	// 开始验证
+	return govalidator.New(opts).ValidateStruct()
+}
