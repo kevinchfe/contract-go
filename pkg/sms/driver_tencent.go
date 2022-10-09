315a866b86710ca3be0ba0a7ea889f89ccf30396
陈飞 <chenfei@360tianma.com>
2022-10-09 23:59:52 +0800

    发送短信(sms 包)

---
+++ pkg/sms/driver_tencent.go
@@ -0,0 +1,10 @@
+package sms
+
+// Tencent 实现sms.Driver interface
+type Tencent struct {
+}
+
+//func (s *Aliyun) Send(phone string, message Message, config map[string]string) bool {
+//	smsClient := aliyunsmsclient.New("http://dysmsapi.aliyuncs.com/")
+//	templateParam, err := json.Marshal(message.Data)
+//}
