06933dd34bf15583a73c86522da21c542b9554b9
陈飞 <chenfei@360tianma.com>
2022-10-12 20:37:54 +0800

    guest 中间件

---
+++ app/http/middlewares/guest_jwt.go
@@ -0,0 +1,22 @@
+package middlewares
+
+import (
+	"contract/pkg/jwt"
+	"contract/pkg/response"
+	"github.com/gin-gonic/gin"
+)
+
+// GuestJWT 强制使用游客身份访问
+func GuestJWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if len(c.GetHeader("Authorization")) > 0 {
+			_, err := jwt.NewJWT().ParserToken(c)
+			if err == nil {
+				response.Unauthorized(c, "请使用游客身份访问")
+				c.Abort()
+				return
+			}
+		}
+		c.Next()
+	}
+}
