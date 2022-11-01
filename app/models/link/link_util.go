e37a6475a6f9dda27d23e09fb1a99f041edf3588
陈飞 <chenfei@360tianma.com>
2022-11-01 20:15:05 +0800

    缓存友情链接列表

--- app/models/link/link_util.go
+++ app/models/link/link_util.go
@@ -1,40 +1,57 @@
 package link
 
 import (
-    "contract/pkg/app"
-    "contract/pkg/database"
-    "contract/pkg/paginator"
-    "github.com/gin-gonic/gin"
+	"contract/pkg/app"
+	"contract/pkg/cache"
+	"contract/pkg/database"
+	"contract/pkg/helpers"
+	"contract/pkg/paginator"
+	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func Get(idstr string) (links Link) {
-    database.DB.Where("id", idstr).First(&links)
-    return
+	database.DB.Where("id", idstr).First(&links)
+	return
 }
 
 func GetBy(field, value string) (links Link) {
-    database.DB.Where("? = ?", field, value).First(&links)
-    return
+	database.DB.Where("? = ?", field, value).First(&links)
+	return
 }
 
 func All() (links []Link) {
-    database.DB.Find(&links)
-    return
+	database.DB.Find(&links)
+	return
 }
 
 func IsExist(field, value string) bool {
-    var count int64
-    database.DB.Model(Link{}).Where("? = ?", field, value).Count(&count)
-    return count > 0
+	var count int64
+	database.DB.Model(Link{}).Where("? = ?", field, value).Count(&count)
+	return count > 0
 }
 
 func Paginate(c *gin.Context, perPage int) (links []Link, paging paginator.Paging) {
-    paging = paginator.Paginate(
-        c,
-        database.DB.Model(Link{}),
-        &links,
-        app.V1URL(database.TableName(&Link{})),
-        perPage,
-    )
-    return
+	paging = paginator.Paginate(
+		c,
+		database.DB.Model(Link{}),
+		&links,
+		app.V1URL(database.TableName(&Link{})),
+		perPage,
+	)
+	return
+}
+
+func AllCached() (links []Link) {
+	cacheKey := "links:all"
+	expireTime := 120 * time.Minute
+	cache.GetObject(cacheKey, &links)
+	if helpers.Empty(links) {
+		links = All()
+		if helpers.Empty(links) {
+			return links
+		}
+		cache.Set(cacheKey, links, expireTime)
+	}
+	return
 }
