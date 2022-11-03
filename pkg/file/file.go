ed126252622278823bb51c3676040fe126cddfc0
陈飞 <chenfei@360tianma.com>
2022-11-03 20:52:46 +0800

    图片裁剪

--- pkg/file/file.go
+++ pkg/file/file.go
@@ -5,6 +5,7 @@ import (
 	"contract/pkg/auth"
 	"contract/pkg/helpers"
 	"fmt"
+	"github.com/disintegration/imaging"
 	"github.com/gin-gonic/gin"
 	"mime/multipart"
 	"os"
@@ -50,7 +51,24 @@ func SaveUploadAvatar(c *gin.Context, file *multipart.FileHeader) (string, error
 		return avatar, err
 	}
 
-	return avatarPath, nil
+	// 裁剪图片
+	img, err := imaging.Open(avatarPath, imaging.AutoOrientation(true))
+	if err != nil {
+		return avatar, err
+	}
+	resizeAvatar := imaging.Thumbnail(img, 256, 256, imaging.Lanczos)
+	resizeAvatarName := randomNameFromUploadFile(file)
+	resizeAvatarPath := publicPath + dirName + resizeAvatarName
+	err = imaging.Save(resizeAvatar, resizeAvatarPath)
+	if err != nil {
+		return avatar, err
+	}
+	// 删除老文件
+	err = os.Remove(avatarPath)
+	if err != nil {
+		return avatar, err
+	}
+	return dirName + resizeAvatarName, nil
 }
 
 func randomNameFromUploadFile(file *multipart.FileHeader) string {
